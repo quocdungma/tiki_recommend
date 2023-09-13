bfd70166eeba4bd609bd2cd3965058610d930231
quocdungma <quocdungma@gmail.com>
2023-09-13 11:29:31 +0700

    a

---
+++ app.py
@@ -0,0 +1,181 @@
+
+import streamlit as st
+from recommender import recommender_gensim, recommend_for_new_product_gensim, preprocess_input, get_purchased_products, recommend_products_user
+import pandas as pd
+import os
+import json
+
+# Load data
+product_data = pd.read_csv('product_data.csv')
+product_data = product_data.rename(columns={'rating': 'product_rating'})
+
+product_data_1 = pd.read_csv('ProductRaw.csv')
+for col in product_data_1.columns:
+    if col not in ['item_id', 'rating', 'price']:
+        product_data_1[col] = product_data_1[col].astype(str)
+directory_path = "recommendations.json"
+
+# Tạo một danh sách để chứa các đối tượng JSON
+json_list = []
+
+# Lặp qua tất cả các tệp trong thư mục
+for filename in os.listdir(directory_path):
+    # Kiểm tra xem tệp có phải là tệp JSON không
+    if filename.endswith(".json"):
+        # Xây dựng đường dẫn đầy đủ đến tệp
+        file_path = os.path.join(directory_path, filename)
+        
+        # Mở và đọc tệp JSON
+        with open(file_path, 'r') as f:
+            data_str = f.read()
+            
+            # Chia chuỗi thành từng đối tượng JSON riêng lẻ và thêm vào danh sách
+            for json_str in data_str.split('\n'):
+                if json_str.strip():  # Kiểm tra xem chuỗi có khác rỗng không
+                    data = json.loads(json_str)
+                    json_list.append(data)
+# Chuyển danh sách các đối tượng JSON thành một DataFrame
+df = pd.DataFrame(json_list)
+
+df['customer_id'] = df['customer_id'].astype(int)
+df['product_id'] = df['product_id'].astype(int)
+
+st.title('Product Recommendation System')
+
+if st.sidebar.button('Giới Thiệu Mô Hình Khuyến Nghị'):
+    st.header('Giới Thiệu Mô Hình Khuyến Nghị')
+
+    st.markdown("""
+    Mô hình khuyến nghị sản phẩm của chúng tôi dựa trên hai phương pháp chính:
+
+    1. **Content-Based Filtering**: Phương pháp này khuyến nghị sản phẩm dựa trên đặc điểm của sản phẩm (Sử dụng thuật toán Gensim). Trong ứng dụng này, chúng tôi sử dụng phương pháp này để khuyến nghị sản phẩm dựa trên ID hoặc tên sản phẩm.
+
+    2. **Collaborative Filtering**: Phương pháp này khuyến nghị sản phẩm dựa trên sự tương tác giữa người dùng và sản phẩm (Sử dụng mô hình ALS). Trong ứng dụng này, chúng tôi sử dụng phương pháp này để khuyến nghị sản phẩm dựa trên ID khách hàng.
+
+    Dưới đây là một hình ảnh minh họa về hai phương pháp này:
+    """)
+
+    st.image('https://www.researchgate.net/profile/Marwa-Mohamed-54/publication/331063850/figure/fig3/AS:729493727621125@1550936266704/Content-based-filtering-and-Collaborative-filtering-recommendation.ppm')
+
+# Tạo thanh lựa chọn số lượng khuyến nghị
+num_recommendations = st.sidebar.slider('Select number of products to display', 5, 50, 5)
+
+# Tạo nút lựa chọn giữa 3 mô hình
+recommend_option = st.sidebar.radio("Choose an option to recommend products:", ('Recommend by ID', 'Recommend by Name', 'Recommend for Customer ID'))
+
+if recommend_option == 'Recommend by ID':
+    # Allow user to input a product ID
+    product_id = st.text_input('Nhập ID sản phẩm:', '')
+    st.write('Ví dụ: 38458616, 14497425, 60228865,...')
+
+    if product_id:
+        try:
+          product_id = int(product_id)
+          # Get product details
+          product_details = product_data[product_data['item_id'] == product_id]
+          if not product_details.empty:
+
+            st.markdown(f"**Product Name:** {product_details['name'].values[0]}")
+            st.markdown(f"**Price:** {product_details['price'].values[0]}")
+            st.markdown(f"**Rating:** {product_details['rating'].values[0]}")
+
+            # Display product image if the URL is available
+            image_url = product_details['image'].values[0]
+            if image_url:
+                st.image(image_url)
+
+            # Get recommendations
+            recommendations = recommender_gensim(product_id, num_recommendations)
+
+            if recommendations is not None:
+                st.write('Recommendations:')
+
+                # Create a grid to display products in rows of 5
+                for start in range(0, len(recommendations), 5):
+                    end = start + 5
+                    cols = st.columns(5)
+                    for i, row in enumerate(recommendations.iloc[start:end].iterrows()):
+                        index, data = row
+                        # Display product image if the URL is available
+                        if data['image']:
+                            cols[i].image(data['image'])
+
+                        cols[i].markdown(f"**Price:** {data['price']}")
+                        cols[i].markdown(f"**Rating:** {data['rating']}")
+                        cols[i].markdown(f"**Name:** {data['name']}")
+
+                        # Create a button to show description
+                        if cols[i].button('Show Description', key=data['item_id']):
+                            cols[i].write(f"Description: {data['description']}")
+          else:
+            st.write('Product not found.')
+        except ValueError:
+          st.write('Please enter a valid product ID.')
+elif recommend_option == 'Recommend by Name':
+    # Allow user to input a product name
+    product_name = st.text_input('Nhập mô tả sản phẩm:', '')
+
+    if product_name:
+      # Tiền xử lý product_name trước khi gọi hàm recommend_for_new_product_gensim
+      processed_product_name = preprocess_input(product_name)
+
+      # Get recommendations
+      recommendations = recommend_for_new_product_gensim(processed_product_name, num_recommendations)
+
+      st.write('Recommendations:')
+        # Create a grid to display products in rows of 5
+      for start in range(0, len(recommendations), 5):
+          end = start + 5
+          cols = st.columns(5)
+          for i, row in enumerate(recommendations.iloc[start:end].iterrows()):
+                index, data = row
+                # Display product image if the URL is available
+                if data['image']:
+                    cols[i].image(data['image'])
+
+                cols[i].markdown(f"**Price:** {data['price']}")
+                cols[i].markdown(f"**Rating:** {data['rating']}")
+                cols[i].markdown(f"**Name:** {data['name']}")
+
+                # Create a button to show description
+                if cols[i].button('Show Description', key=data['item_id']):
+                    cols[i].write(f"Description: {data['description']}")
+
+elif recommend_option == 'Recommend for Customer ID':
+    customer_id = st.text_input('Enter customer ID:', '')
+
+    if customer_id:
+        try:
+            customer_id = int(customer_id)
+            
+            # Display purchased products
+            st.write('Purchased Products:')
+            purchased_products = get_purchased_products(customer_id, df, product_data)
+            if purchased_products is not None and not purchased_products.empty:
+                st.markdown(purchased_products.to_html(escape=False), unsafe_allow_html=True)
+            else:
+                st.write('No purchased products found.')
+
+            # Get recommendations using recommend_products function
+            recommendations = recommend_products_user(customer_id, df, product_data, num_recommendations)
+
+            if recommendations is not None:
+                st.write('Recommendations:')
+                cols = st.columns(5)
+
+                for i, row in enumerate(recommendations.iterrows()):
+                    index, data = row
+
+                    # Display product image if available
+                    if not pd.isnull(data['image']):
+                        cols[i % 5].image(data['image'])
+
+                    cols[i % 5].markdown(f"**Price:** {data['price']}")
+                    cols[i % 5].markdown(f"**Rating:** {data['product_rating']}")
+                    cols[i % 5].markdown(f"**Name:** {data['name']}")
+
+                    with cols[i % 5].expander("Description"):
+                        st.write(data['description'])
+
+        except ValueError:
+            st.write('Please enter a valid customer ID.')
---
+++ recommender.py
@@ -0,0 +1,154 @@
+
+import time
+import pandas as pd
+import pickle
+import nltk
+
+nltk.download('punkt')
+from nltk.tokenize import word_tokenize
+import re
+
+# Lấy danh sách stop words từ file
+with open('vietnamese-stopwords.txt', 'r', encoding='utf-8') as file:
+    stop_words = file.read().split('\n')
+
+# Load pre-saved objects
+with open('dictionary.pkl', 'rb') as f:
+    dictionary = pickle.load(f)
+
+with open('tfidf_model.pkl', 'rb') as f:
+    tfidf = pickle.load(f)
+
+with open('similarity_index.pkl', 'rb') as f:
+    index = pickle.load(f)
+
+# Load product data
+product_data = pd.read_csv('product_data.csv')
+
+def recommender_gensim(product_id, num_recommendations):
+    start_time = time.time()  # Start the timer
+
+    # Retrieve product details based on product_id
+    product_selection = product_data[product_data['item_id'] == product_id]
+
+    # Check if the product is not found
+    if product_selection.empty:
+        print("Không tìm thấy sản phẩm với ID:", product_id)
+        return None
+
+    # Extract the product description
+    name_description_pre = product_selection['content_wt'].to_string(index=False)
+
+    # Convert search word into Sparse Vectors
+    view_product = name_description_pre.lower().split()
+    kw_vector = dictionary.doc2bow(view_product)
+
+    # Similarity calculation
+    sim = index[tfidf[kw_vector]]
+
+    # Create dataframe to store results
+    df_result = pd.DataFrame({'id': range(len(sim)), 'score': sim})
+
+    # Get the highest scores
+    highest_scores = df_result.sort_values(by='score', ascending=False).head(num_recommendations+1)
+
+    # Extract the corresponding rows from product_data
+    products_find = product_data[product_data.index.isin(highest_scores['id'])]
+    results = products_find[['item_id', 'name', 'image', 'price', 'rating', 'description']]
+
+    # Merge results and scores, and sort by score
+    merged_results = pd.merge(results, highest_scores, left_index=True, right_on='id').sort_values(by='score', ascending=False)
+    merged_results = merged_results[merged_results['item_id'] != product_id]
+
+
+    # Extract recommended items (excluding the searched product)
+    recs = merged_results[merged_results['item_id'] != product_id].values
+
+    # Print the results
+    print('Recommending ' + str(num_recommendations) + ' products similar to ' + product_selection['name'].values[0] + '...')
+    print('*' * 96)
+    for rec in recs:
+        print('Recommended: product id: ' + str(int(rec[0])) + ', ' + rec[1] + ' (score: ' + str(rec[6]) + ')')
+
+    end_time = time.time()  # End the timer
+
+    print("\nTime taken for recommendation: {:.2f} seconds".format(end_time - start_time))
+
+    return merged_results
+
+# Hàm tiền xử lý đầu vào
+def preprocess_input(input_text):
+    input_text = re.sub('[^a-zA-Z0-9 \n\.]', '', input_text)
+    input_text = input_text.lower()
+    input_text = word_tokenize(input_text)
+    input_text = ' '.join([word for word in input_text if word not in stop_words])
+    return input_text
+
+def recommend_for_new_product_gensim(product_name, num_recommendations=5):
+    try:
+        # Tiền xử lý đầu vào
+        processed_input = preprocess_input(product_name)
+
+        # Chuyển đổi đầu vào thành vector đặc trưng
+        query_bow = dictionary.doc2bow(processed_input.split())
+
+        # Tính toán độ tương tự sử dụng mô hình Gensim
+        query_tfidf = tfidf[query_bow]
+        sims = index[query_tfidf]
+
+        # Lấy ra các sản phẩm tương tự
+        similar_items = sorted(enumerate(sims), key=lambda x: x[1], reverse=True)[:num_recommendations]
+
+        # Tạo một danh sách để lưu trữ thông tin sản phẩm
+        product_info_list = []
+
+        # Hiển thị kết quả
+        print(f"Recommending {num_recommendations} products similar to {product_name}:")
+        for item in similar_items:
+            product_info = product_data.iloc[item[0]][['item_id', 'name', 'image', 'price', 'rating', 'description']]
+            product_info_list.append(product_info)
+            print(f"Product ID: {product_info['item_id']}, Product Name: {product_info['name']}, Score: {item[1]}")
+
+        # Chuyển đổi danh sách thông tin sản phẩm thành DataFrame
+        recommended_df = pd.DataFrame(product_info_list)
+        return recommended_df
+    except Exception as e:
+        print(f"Có lỗi xảy ra: {e}")
+        return None
+import os
+import json
+import pandas as pd
+
+def get_purchased_products(customer_id, reviews_data, product_data):
+    # Step 1: Filter data
+    purchased_products = reviews_data[reviews_data['customer_id'] == customer_id][['product_id', 'rating']].drop_duplicates()
+
+    # Step 2: Join DataFrames
+    purchased_products_details = purchased_products.merge(product_data, left_on='product_id', right_on='item_id', how='inner')
+
+    # Step 3: Select necessary columns
+    purchased_products_details = purchased_products_details[['item_id', 'name', 'image', 'price', 'product_rating', 'description', 'rating']]
+    
+    # Step 4: Embed images in DataFrame
+    purchased_products_details['image'] = purchased_products_details['image'].apply(lambda x: f'<img src="{x}" width="100px" />')
+
+    return purchased_products_details
+
+def recommend_products_user(customer_id, df, products_df, num_recommendations=10):
+
+    customer_id = int(customer_id) 
+
+    # Bước 1: Xác định các sản phẩm đã được khuyến nghị
+    recommended_products_already = df[df['customer_id'] == customer_id][['product_id', 'rating']].head(num_recommendations)
+
+    # Bước 2: Tìm sản phẩm tương tựht
+    # Ở đây, chúng ta chỉ đơn giản lấy những sản phẩm chưa được khuyến nghị từ tập sản phẩm
+    similar_products = products_df[products_df['item_id'].isin(recommended_products_already['product_id'])]
+
+    # Bước 3: Sắp xếp sản phẩm dựa trên đánh giá từ df
+    # Ở đây, chúng ta sáp nhập cột đánh giá từ df vào similar_products và sau đó sắp xếp chúng
+    similar_products = similar_products.merge(recommended_products_already, left_on='item_id', right_on='product_id', how='left')
+    recommended_products = similar_products.sort_values(by='rating', ascending=False)
+    
+    # Bước 4: Trả về danh sách khuyến nghị
+    return recommended_products[['name','description','product_rating','price','image']].head(num_recommendations)
